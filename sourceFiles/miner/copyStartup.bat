01bf75bc3ae66eaed65f616b072511a56e2657de
Camp Steiner <joefakocamp@gmail.com>
2020-03-29 20:19:37 -0500

    Writing code in batch instead of unix shell

--- sourceFiles/miner/copyStartup.bat
+++ sourceFiles/miner/copyStartup.bat
@@ -5,7 +5,7 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Start Menu\Programs\Startup\charitasStartup.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "%PWD%\CharitasCGPU.bat" >> %SCRIPT%
+echo oLink.TargetPath = "%CD%\CharitasCGPU.bat" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
