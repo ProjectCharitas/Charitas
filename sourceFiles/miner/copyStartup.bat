df64bd24bed82fff10482ae9cf72456fc4bcff59
Camp Steiner <joefakocamp@gmail.com>
2020-03-29 19:59:58 -0500

    Fixing startup (?)

--- sourceFiles/miner/copyStartup.bat
+++ sourceFiles/miner/copyStartup.bat
@@ -3,7 +3,7 @@
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-echo sLinkFile = "C:\Users\%USERNAME%\Start Menu\Programs\Startup\charitasStartup.lnk" >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Start Menu\Programs\Startup\charitasStartup.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "%PWD%\CharitasCGPU.bat" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
